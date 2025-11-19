6c82904376129326b1e21ad18f6c72a2ada8464a
PC <C:\Users\PC\AppData\Roaming\The Bat!>
2025-11-19 10:58:18 +0700

    Ваше описание изменений

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCPpQIiE0XgTk7lUkm9xWhZ7aHBhJTxX-Y",
+  authDomain: "parallax-ai-baff3.firebaseapp.com",
+  projectId: "parallax-ai-baff3",
+  storageBucket: "parallax-ai-baff3.firebasestorage.app",
+  messagingSenderId: "386155070564",
+  appId: "1:386155070564:web:12fdf1880599e4365b4b71"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
